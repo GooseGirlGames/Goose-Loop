b51e46d0d75ced6ff109193ed3464a037e8ca0c6
YoFionn <ericksoncooperation@gmail.com>
2021-12-08 00:01:19 +0100

    shootgoose goose can shoot good geese

--- G-Loop/Assets/Scenes/BloodyTest/Shoot.cs
+++ G-Loop/Assets/Scenes/BloodyTest/Shoot.cs
@@ -15,30 +15,23 @@ public class Shoot : MonoBehaviour
         
     }
     IEnumerator Reload(){
+            reload = true;
             yield return new WaitForSeconds(5);
             reload = false;
             bullet_count = 1; 
     }
 
     void Update()
-    {   if(bullet_count%(bullets+1) != 0){
-            reload = false;
-        }
-        else{
-            reload = true;
-        }    
-        if(reload){
-            StartCoroutine(Reload());
-        }
-        else{
-            if(Input.GetButtonDown("Fire1")){
-                gun.SetActive(true);
-                bullet_count++; 
-                GameObject bullet = Instantiate(gun, spawn.transform.position, Quaternion.identity) as GameObject;
-                bullet.GetComponent<Rigidbody>().AddForce(transform.forward *1000);
-                Debug.Log(bullet_count);
+    { 
+        if(Input.GetButtonDown("Fire1") && !reload){
+            GameObject bullet = Instantiate(gun, spawn.transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward *1000);
+            gun.SetActive(true);
+            bullet_count++; 
+            if(bullet_count%(bullets+1) == 0){
+                StartCoroutine(Reload());
             }
+            Debug.Log(bullet_count);
         }
-        
     }
 }
