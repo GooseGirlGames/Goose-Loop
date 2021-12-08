8bec3b6e1e44bf0d3cdf09f45e43131068785dba
Mielein <marie.trojan@uni-weimar.de>
2021-12-08 14:15:50 +0100

    bullets

--- G-Loop/Assets/Scenes/BloodyTest/Shoot.cs
+++ G-Loop/Assets/Scenes/BloodyTest/Shoot.cs
@@ -7,14 +7,22 @@ public class Shoot : MonoBehaviour
     public GameObject gun;
     public GameObject spawn;
     public GameObject reload_warning;
+    
+    public GameObject bullet1;
+    public GameObject bullet2;
+    public GameObject bullet3;
 
+    private GameObject[] bullet_image;
     public int bullets;
     private int bullet_count = 1;
     private bool reload = false;
 
     // Start is called before the first frame update
     void Start(){
-        
+        bullet_image = new GameObject[3];
+        bullet_image[0] = bullet1;
+        bullet_image[1] = bullet2;
+        bullet_image[2] = bullet3;
     }
 
     IEnumerator Reload(){
@@ -25,6 +33,10 @@ public class Shoot : MonoBehaviour
             reload_warning.SetActive(false);
             reload = false;
             bullet_count = 1; 
+            for(int i = 0; i < bullet_image.Length; i++){
+                bullet_image[i].SetActive(true);
+            }
+            
     }
 
     void Update()
@@ -33,6 +45,7 @@ public class Shoot : MonoBehaviour
             GameObject bullet = Instantiate(gun, spawn.transform.position, Quaternion.identity) as GameObject;
             bullet.GetComponent<Rigidbody>().AddForce(transform.forward *1000);
             gun.SetActive(true);
+            bullet_image[bullet_count-1].SetActive(false);
             bullet_count++; 
             if(bullet_count%(bullets+1) == 0){
                 StartCoroutine(Reload());
