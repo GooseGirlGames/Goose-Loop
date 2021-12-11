2a92a4b9ec6485c8c32705bcd1b289441304ec00
Jakob Ruckel <jakob@ruckel.de>
2021-12-11 21:59:56 +0100

    boom sound

--- G-Loop/Assets/Scripts/ExplodedFeathers.cs
+++ G-Loop/Assets/Scripts/ExplodedFeathers.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class ExplodedFeathers : MonoBehaviour {
     new public ParticleSystem particleSystem;
+    public AudioSource audioSource;
+    public AudioClip explode;
     public const int FEATHER_COUNT = 400;
     void Start() {
         particleSystem.Stop();
     }
 
     public void Explode() {
+        audioSource.PlayOneShot(explode);
         particleSystem.Emit(FEATHER_COUNT);
     }
 }
