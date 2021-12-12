da755a7b260be2556dee6909385f25cbb4c057de
Jakob Ruckel <jakob@ruckel.de>
2021-12-12 04:52:07 +0100

    Add VHS for FF

---
+++ G-Loop/Assets/Scripts/Ghoostling/VHSEffectToggle.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VHSEffectToggle : MonoBehaviour {
+    public MonoBehaviour effect;
+    private GhoostlingManager gman;
+    private void Start() {
+        gman = GhoostlingManager.GetInstance();
+    }
+    void Update() {
+        effect.enabled = gman.IsFastForwarding();
+    }
+}
---
+++ G-Loop/Assets/Scripts/UI/MainUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour {
+    public Canvas canvas;
+    public Image fastForward;
+    private GhoostlingManager gman;
+    // Start is called before the first frame update
+    void Start() {
+        gman = GhoostlingManager.GetInstance();
+    }
+    public static MainUI GetInstance() {
+        var uis = GameObject.FindObjectsOfType<MainUI>();
+        if (uis.Length > 1) {
+            Debug.LogWarning("There are multiple MainUI."
+                    + "Please make sure only one exists per scene.");
+        } else if (uis.Length == 0) {
+            Debug.LogError("No MainUI is present."
+                    + "Please make sure one exists in the scene.");
+        }
+        return uis[0];
+    }
+    // Update is called once per frame
+    private void OnGUI() {
+        fastForward.enabled = gman.IsFastForwarding();
+    }
+}
