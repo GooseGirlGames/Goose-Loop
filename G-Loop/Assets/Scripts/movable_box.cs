f945bd2aede573b48d3d9c1c523dfa7e051eacb8
YoFionn <ericksoncooperation@gmail.com>
2021-12-13 00:46:33 +0100

    moveabal stuff

--- G-Loop/Assets/Scripts/movable_box.cs
+++ G-Loop/Assets/Scripts/movable_box.cs
@@ -4,29 +4,60 @@ using UnityEngine;
 
 public class movable_box : MonoBehaviour
 {
-    private Collider[] moveZone;
-    public GameObject zone_holder;
-    private Rigidbody body;
+    public LayerMask FloatLayer;
+    private GameObject body;
     private bool allowed_to_move = true;
     // Start is called before the first frame update
 
     private void Start() {
-        moveZone = zone_holder.GetComponents<Collider>();
-        body = GetComponent<Rigidbody>();
+        body = gameObject;
+    }
+
+    private void zoneCheck(){
+        if(Physics.CheckSphere(body.transform.position, 0.1f , FloatLayer)){
+            allowed_to_move = true;
+        }
+        else{
+            allowed_to_move = false;
+        }
+    }
+    private void Update() {
+        zoneCheck();
     }
     public void move_x(){
+        Debug.Log("in zone: " + allowed_to_move);
         if(allowed_to_move){
-            Debug.Log("is moving");
             body.transform.Translate(0.1f, 0f, 0f);
+        }
+        else{
+        body.transform.Translate(-0.1f, 0f, 0f);
         }
     }
     public void move_x_back(){
-
+        Debug.Log("in zone: " + allowed_to_move);
+        if(allowed_to_move){
+            body.transform.Translate(-0.1f, 0f, 0f);
+        }
+        else{
+        body.transform.Translate(0.1f, 0f, 0f);
+        }
     }
     public void move_z(){
-        
+        Debug.Log("in zone: " + allowed_to_move);
+        if(allowed_to_move){
+            body.transform.Translate(0f, 0f, 0.1f);
+        }
+        else{
+        body.transform.Translate(0f, 0f, -0.1f);
+        }
     }
     public void move_z_back(){
-        
+        Debug.Log("in zone: " + allowed_to_move);
+        if(allowed_to_move){
+            body.transform.Translate(0f, 0f, -0.1f);
+        }
+        else{
+        body.transform.Translate(0f, 0f, 0.1f);
+        }
     }
 }
