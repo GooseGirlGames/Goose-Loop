03bf7d720c2ce98aeb0a44cee32c6753c7f9a7cf
YoFionn <ericksoncooperation@gmail.com>
2021-12-12 20:18:35 +0100

    button animation if standig on it

--- G-Loop/Assets/Scripts/Trigger/button_push.cs
+++ G-Loop/Assets/Scripts/Trigger/button_push.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class button_push : MonoBehaviour
 {
+    private Animator button_animator;
+    private Collider button;
+    public bool active = false;
+    
     
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        button_animator = GetComponent<Animator>();
+        button = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        active = true;
+    }
+    private void OnTriggerExit(Collider other) {
+        active = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        button_animator.SetBool("pressed", active);
     }
 }
