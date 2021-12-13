c37c45b37a5485d0e4e240a4d02350b84ef7880e
YoFionn <ericksoncooperation@gmail.com>
2021-12-13 04:19:48 +0100

    the pully is working now

--- G-Loop/Assets/Scripts/Trigger/pulley_down.cs
+++ G-Loop/Assets/Scripts/Trigger/pulley_down.cs
@@ -6,21 +6,33 @@ public class pulley_down : MonoBehaviour
 {
     // Start is called before the first frame update
     private Animator pully_animator;
+    public Animator going_up;
     public int gooseCount;
+    public bool used = false;
     
     void Start()
     {
         pully_animator = GetComponent<Animator>();
     }
-    private void OnCollisionEnter(Collision other) {
-        gooseCount++;
+    private void OnTriggerEnter(Collider other) {
+        var cc = other.GetComponentInChildren<CharacterController>();
+        if (cc) {
+            //Debug.Log("Goose enter: " + other);
+            gooseCount++;
+        }
     }
-    private void OnCollisionExit(Collision other) {
-        gooseCount--;
+    private void OnTriggerExit(Collider other) {
+        var cc = other.GetComponentInChildren<CharacterController>();
+        if (cc) {
+            //Debug.Log("Goose enter: " + other);
+            gooseCount--;
+        }
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         pully_animator.SetInteger("passangers", gooseCount);
+        //Debug.Log("Goose on board:" + gooseCount);
+        going_up.SetInteger("passangers", gooseCount);
     }
 }
