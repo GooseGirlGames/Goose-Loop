c922b26d59401925cbd6cfe6e73df1c291b32394
Jakob Ruckel <jakob@ruckel.de>
2021-12-19 03:08:24 +0100

    Update skybox

--- G-Loop/Assets/Textures/skybox/skybox_copy_images.sh
+++ G-Loop/Assets/Textures/skybox/skybox_copy_images.sh
@@ -2,7 +2,7 @@
 
 cp /tmp/0001.png bot.png
 cp /tmp/0002.png front.png
-cp /tmp/0003.png left.png
+cp /tmp/0005.png left.png
 cp /tmp/0004.png back.png
-cp /tmp/0005.png right.png
+cp /tmp/0003.png right.png
 cp /tmp/0006.png top.png
